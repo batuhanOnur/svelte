ed6b28366ea448431aaf592686194bac5b52df98
batuhan <batuhanonur29@gmail.com>
2020-04-13 15:20:59 +0300

    conditional styles

--- myproject/src/Modal.svelte
+++ myproject/src/Modal.svelte
@@ -1,12 +1,30 @@
 <script>
+let showModal = true;
 </script>
 
 
-
+{#if showModal}
 <div class="backdrop">
  <div class="modal">
   <p>Sign Up</p>
  </div>
 </div>
+{/if}
 
-<style></style>
+<style>
+  .backdrop {
+      width: 100%;
+      height: 100%;
+      position: fixed;
+      background: rgba(0,0,0,0.8);
+  }
+
+  .modal {
+      padding: 10px;
+      border-radius: 10px;
+      max-width: 400px;
+      margin: 10% auto;
+      text-align: center;
+      background: white;
+  }
+</style>
